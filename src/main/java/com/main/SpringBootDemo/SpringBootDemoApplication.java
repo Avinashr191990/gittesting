09b804181c77a3109fb0ed3c7269145416acbf2f
Avinash <avinash.ramakkapet@gmail.com>
2022-03-02 12:14:10 +0530

    testing commit-1

--- src/main/java/com/main/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/main/SpringBootDemo/SpringBootDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("testing- 1st commit");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
