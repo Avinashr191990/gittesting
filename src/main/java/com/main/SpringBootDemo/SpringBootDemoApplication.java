1371bc7da3fd4eac565278b4aec6588d2d447463
Avinash <avinash.ramakkapet@gmail.com>
2022-03-02 12:03:34 +0530

    src added

---
+++ src/main/java/com/main/SpringBootDemo/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.main.SpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("");
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+
+}
